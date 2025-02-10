1e75409badd543e43e2caf534148475aad0dfd38
SoudaniLyes <125369497+Lyes-Syel@users.noreply.github.com>
2025-02-10 11:37:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+@app.route('/about')
+def about():
+  return 'Bienvenue sur la page À propos de Flask sur Render!'
